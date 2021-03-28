d62364651f770d038ad9c2efdb460ef066eab967
Dimitri Michelis <80400914+dmichelis@users.noreply.github.com>
2021-03-28 09:11:32 -0500

    Add files via upload
    definitely not hey erin this time

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,6 +1,7 @@
+class Parking_Garage:    
     def __init__(self,parkingSpaceAvail,ticketsAvail,currentTicket):
-        self.parkingSpaceAvail = [i for i in range(1,11+1)]
-        self.ticketsAvail = [i for i in range(1,11+1)]
+        self.parkingSpaceAvail = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        self.ticketsAvail = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         # self.ticketsAvail = [1,2,3,4,5,6,7,8,9,10]
         # self.currentTicket = {'ticket_num': '', 'status': ''}
         self.currentTicket = {
@@ -30,8 +31,8 @@
         if self.ticketsAvail != []:
 
             print(f'Please take your ticket {last_ticket_popped}')
-            tickets_used.append(last_ticket_popped)
-            spaces_used.append(last_space_popped)
+            #tickets_used.append(last_ticket_popped)
+            #spaces_used.append(last_space_popped)
 
             # currentTicket = dict.fromkeys(str(last_ticket_popped))
             # print(currentTicket)
@@ -50,22 +51,42 @@
 
 
     def payForParking(self):
+        global ticket_num
+        global payment_amt
         ticket_num = int(input("Please enter your ticket number: "))
         payment_amt = int(input("Please enter your payment amount: "))
         
 
+        #if payment_amt == "":
+            #self.currentTicket[ticket_num] = 'not paid'
+        
         if payment_amt != "":
             print("Your ticket has been paid. Please exit the parking garage within the next 15 minutes.")
 
         if payment_amt >= 0:
             self.currentTicket[ticket_num] = 'paid'
+           
 
         else:
-            print('Thank you for your business! Please exit the parking garage within the next 15 minutes.')
+            self.currentTicket[ticket_num] = 'not paid'
 
         print(self.currentTicket)
+    
+
+    def leaveGarage(self):
+        if self.currentTicket[ticket_num] == 'paid':
+            self.parkingSpaceAvail.append(ticket_num)
+            self.ticketsAvail.append(ticket_num)
+            print("Thank you, have a nice day!")
 
 
+            print(*self.ticketsAvail)
+            print(*self.parkingSpaceAvail)
+            print(self.currentTicket)
+        
+        if self.currentTicket[ticket_num] != 'paid':
+            ticket_num2 = int(input("Please enter your ticket number: "))
+            
 
 
 
@@ -87,4 +108,8 @@ def run():
             parkingToday.payForParking()
             # break
 
+        elif park_today.lower() == 'finish':
+            parkingToday.leaveGarage()
+
 run()
+
